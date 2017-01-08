893e95b30361aade388f926fb620a07c5e3cad30
Denis Koronchik <denis.koronchik@gmail.com>
2017-01-08 18:51:47 +0300

    [utils] Add input constants for a mouse

---
+++ app/js/input.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  // according to https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+  Mouse: {
+    LeftButton:     0,
+    MiddleButton:   1,
+    RightButton:    2,
+    FourthButton:   3,
+    FifthButton:    4
+  }
+}
